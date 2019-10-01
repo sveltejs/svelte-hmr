e01c93ab017914d375dceffc5d4c6cef30305e55
rixo <rixo@rixo.fr>
2019-10-01 09:53:00 +0200

    refactor: extract overlay

---
+++ runtime/overlay.js
@@ -0,0 +1,126 @@
+const removeElement = el => el && el.parentNode && el.parentNode.removeChild(el)
+
+const ErrorOverlay = () => {
+  let errors = []
+  let compileError = null
+
+  const errorsTitle = 'Failed to init component'
+  const compileErrorTitle = 'Failed to compile'
+
+  const style = {
+    section: `
+      display: none;
+      position: fixed;
+      top: 0;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      padding: 32px;
+      background: rgba(0, 0, 0, .85);
+      font-family: Menlo, Consolas, monospace;
+      font-size: large;
+      color: rgb(232, 232, 232);
+      overflow: auto;
+    `,
+    h1: `
+      margin-top: 0;
+      color: #E36049;
+      font-size: large;
+      font-weight: normal;
+    `,
+    h2: `
+      margin: 32px 0 0;
+      font-size: large;
+      font-weight: normal;
+    `,
+    pre: ``,
+  }
+
+  const createOverlay = () => {
+    const h1 = document.createElement('h1')
+    h1.style = style.h1
+    const section = document.createElement('section')
+    section.appendChild(h1)
+    section.style = style.section
+    const body = document.createElement('div')
+    section.appendChild(body)
+    const target = document.body
+    target.appendChild(section)
+    return { h1, el: section, body }
+  }
+
+  const setTitle = title => {
+    overlay.h1.textContent = title
+  }
+
+  const show = () => {
+    overlay.el.style.display = 'block'
+  }
+
+  const hide = () => {
+    overlay.el.style.display = 'none'
+  }
+
+  const update = () => {
+    if (compileError) {
+      overlay.body.innerHTML = ''
+      setTitle(compileErrorTitle)
+      const errorEl = renderError(compileError)
+      overlay.body.appendChild(errorEl)
+      show()
+    } else if (errors.length > 0) {
+      overlay.body.innerHTML = ''
+      setTitle(errorsTitle)
+      errors.forEach(({ title, message }) => {
+        const errorEl = renderError(message, title)
+        overlay.body.appendChild(errorEl)
+      })
+      show()
+    } else {
+      hide()
+    }
+  }
+
+  const renderError = (message, title) => {
+    const div = document.createElement('div')
+    if (title) {
+      const h2 = document.createElement('h2')
+      h2.textContent = title
+      h2.style = style.h2
+      div.appendChild(h2)
+    }
+    const pre = document.createElement('pre')
+    pre.textContent = message
+    div.appendChild(pre)
+    return div
+  }
+
+  const addError = (error, title) => {
+    const message = (error && error.stack) || error
+    errors.push({ title, message })
+    update()
+  }
+
+  const clearErrors = () => {
+    errors.forEach(({ element }) => {
+      removeElement(element)
+    })
+    errors = []
+    update()
+  }
+
+  const setCompileError = message => {
+    compileError = message
+    update()
+  }
+
+  const overlay = createOverlay()
+
+  return {
+    addError,
+    clearErrors,
+    setCompileError,
+  }
+}
+
+export default ErrorOverlay
--- runtime/proxy-adapter-dom.js
+++ runtime/proxy-adapter-dom.js
@@ -1,104 +1,7 @@
 /* global document */
+import ErrorOverlay from './overlay'
 
 const removeElement = el => el && el.parentNode && el.parentNode.removeChild(el)
-
-const ErrorOverlay = () => {
-  const errors = []
-
-  const style = {
-    section: `
-      display: none;
-      position: fixed;
-      top: 0;
-      bottom: 0;
-      left: 0;
-      right: 0;
-      padding: 32px;
-      background: rgba(0, 0, 0, .85);
-      font-family: Menlo, Consolas, monospace;
-      font-size: large;
-      color: rgb(232, 232, 232);
-      overflow: auto;
-    `,
-    h1: `
-      margin-top: 0;
-      color: #E36049;
-      font-size: large;
-      font-weight: normal;
-    `,
-    h2: `
-      margin: 32px 0 0;
-      font-size: large;
-      font-weight: normal;
-    `,
-    pre: ``,
-  }
-
-  let el
-
-  const createOverlay = () => {
-    const title = 'Failed to init component'
-    const h1 = document.createElement('h1')
-    h1.textContent = title
-    const section = document.createElement('section')
-    section.appendChild(h1)
-    section.style = style.section
-    h1.style = style.h1
-    const target = document.body
-    target.appendChild(section)
-    return section
-  }
-
-  const show = () => {
-    el.style.display = 'block'
-  }
-
-  const hide = () => {
-    el.style.display = 'none'
-  }
-
-  const popError = wrap => {
-    const index = errors.indexOf(wrap)
-    if (index === -1) return
-    errors.splice(index, 1)
-    if (errors.length === 0) {
-      hide()
-    }
-  }
-
-  const addError = (error, title) => {
-    let removed = false
-
-    const h2 = document.createElement('h2')
-    h2.textContent = title
-    h2.style = style.h2
-    const pre = document.createElement('pre')
-    pre.textContent = (error && error.stack) || error
-    const div = document.createElement('div')
-    div.appendChild(h2)
-    div.appendChild(pre)
-
-    el.appendChild(div)
-
-    const wrap = { error } // ensure unique ref, even if same error instance
-    errors.push(wrap)
-    if (errors.length === 1) {
-      show()
-    }
-    return () => {
-      if (removed) return
-      removed = true
-      removeElement(div)
-      popError(wrap)
-    }
-  }
-
-  el = createOverlay()
-
-  return {
-    addError,
-  }
-}
 
 export default class ProxyAdapterDom {
   constructor(instance) {
@@ -156,13 +59,14 @@ export default class ProxyAdapterDom {
       instance: { debugName },
     } = this
     const title = debugName || err.moduleName || 'Error'
-    this.popError = this.constructor.getErrorOverlay().addError(err, title)
+    this.constructor.getErrorOverlay().addError(err, title)
   }
 
   clearError() {
-    if (this.popError) {
-      this.popError()
-      this.popError = null
-    }
+    this.constructor.getErrorOverlay().clearErrors()
+  }
+
+  static renderCompileError(message) {
+    this.getErrorOverlay().setCompileError(message)
   }
 }
