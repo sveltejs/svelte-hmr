40d9dfdc999e10315b8be13c7f40c5c5723dbb9e
rixo <rixo@rixo.fr>
2021-03-17 02:06:17 +0100

    reimplement absoluteImports option, & add packageImportsVersion

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const createMakeHotFactory = require('./lib/make-hot.js')
 const { resolve } = require('path')
+const { name, version } = require('./package.json')
 
-const resolveAbsoluteImport = target => resolve(__dirname, target)
+const resolveAbsoluteImport = target => '/' + resolve(__dirname, target)
 
-const createMakeHot = createMakeHotFactory({ resolveAbsoluteImport })
+const createMakeHot = createMakeHotFactory({
+  pkg: { name, version },
+  resolveAbsoluteImport,
+})
 
 module.exports = { createMakeHot }
