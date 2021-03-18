7d04e6c60794da6e8bd7e49b8a2b10afb2bf26b8
rixo <rixo@rixo.fr>
2021-03-19 00:20:28 +0100

    fix absolute import

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const createMakeHotFactory = require('./lib/make-hot.js')
 const { resolve } = require('path')
 const { name, version } = require('./package.json')
 
-const resolveAbsoluteImport = target => '/' + resolve(__dirname, target)
+const resolveAbsoluteImport = target => resolve(__dirname, target)
 
 const createMakeHot = createMakeHotFactory({
   pkg: { name, version },
