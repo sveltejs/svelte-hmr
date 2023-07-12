583a446008bd0452aeaf410e4492c835256ba6e5
rixo <rixo@rixo.fr>
2023-07-12 11:06:48 +0200

    make sure we're stopping test properly on failure or crash

--- playground/$test/hmr-macro.js
+++ playground/$test/hmr-macro.js
@@ -74,109 +74,111 @@ export const hmr = (_steps) => {
 
     const { start, stop } = initHmrTest(ctx)
 
-    const { page, edit } = await start({
-      exposeFunction,
-      files: Object.fromEntries(
-        Object.entries(files).map(([file, contents]) => [
-          resolve(file),
-          typeof contents === 'function' ? contents({}) : contents,
-        ])
-      ),
-    })
+    try {
+      const { page, edit } = await start({
+        exposeFunction,
+        files: Object.fromEntries(
+          Object.entries(files).map(([file, contents]) => [
+            resolve(file),
+            typeof contents === 'function' ? contents({}) : contents,
+          ])
+        ),
+      })
 
-    for (const step of steps) {
-      /** @param {(typeof step)['edit']} editSpec */
-      const runEdit = async (editSpec) => {
-        await Promise.all(
-          Object.entries(editSpec).map(([file, arg]) => {
-            const resolveContents = () => {
-              if (typeof arg === 'object') {
-                const template = files?.[file]
-                if (template == null) {
-                  throw new Error(`Cannot edit missing file: ${file}`)
+      for (const step of steps) {
+        /** @param {(typeof step)['edit']} editSpec */
+        const runEdit = async (editSpec) => {
+          await Promise.all(
+            Object.entries(editSpec).map(([file, arg]) => {
+              const resolveContents = () => {
+                if (typeof arg === 'object') {
+                  const template = files?.[file]
+                  if (template == null) {
+                    throw new Error(`Cannot edit missing file: ${file}`)
+                  }
+                  return /** @type {function} */ (template)(arg)
+                } else {
+                  return arg
                 }
-                return /** @type {function} */ (template)(arg)
-              } else {
-                return arg
               }
-            }
-            return edit(resolve(file), resolveContents())
-          })
-        )
-      }
-
-      /**
-       * @param {(typeof step)['expect']} expect
-       * @param {string} name
-       */
-      const runExpect = async (expect, name) => {
-        const expects =
-          typeof expect === 'string'
-            ? [['body', expect]]
-            : Object.entries(expect)
-        for (const [selector, expected] of expects) {
-          const el = await page.$(selector)
-          if (!el) {
-            throw new Error(`Not found in page: ${selector}`)
-          }
-          assert.equal(
-            normalizeHtml(await el.innerHTML()),
-            normalizeHtml(expected),
-            `${name} > ${selector}`
+              return edit(resolve(file), resolveContents())
+            })
           )
         }
-      }
 
-      if ('edit' in step) {
-        await runEdit(step.edit)
-      }
-
-      if ('expect' in step) {
-        await runExpect(step.expect, step.name)
-      }
-
-      if ('steps' in step) {
-        /** @param {TestHmrStepSteps} sub */
-        const runSubStep = async (sub, i = 0) => {
-          if (Array.isArray(sub)) {
-            let i = 0
-            for (const subsub of sub) {
-              await runSubStep(subsub, i++)
+        /**
+         * @param {(typeof step)['expect']} expect
+         * @param {string} name
+         */
+        const runExpect = async (expect, name) => {
+          const expects =
+            typeof expect === 'string'
+              ? [['body', expect]]
+              : Object.entries(expect)
+          for (const [selector, expected] of expects) {
+            const el = await page.$(selector)
+            if (!el) {
+              throw new Error(`Not found in page: ${selector}`)
             }
-          } else if (typeof sub === 'function') {
-            await sub(page)
-          } else {
-            const unknownOptions = new Set(Object.keys(sub))
-            if ('name' in sub) {
-              unknownOptions.delete('name')
-              // TODO
-            }
-            if ('edit' in sub) {
-              unknownOptions.delete('edit')
-              await runEdit(sub.edit)
-            }
-            if ('expect' in sub) {
-              unknownOptions.delete('expect')
-              await runExpect(
-                sub.expect,
-                `${step.name} > ${sub.name || `steps[${i}]`}`
-              )
-            }
-            if (unknownOptions.size > 0) {
-              throw new Error(
-                `Unkown options in step ${step.name}: ${[
-                  // @ts-ignore
-                  ...unknownOptions,
-                ].join(', ')}`
-              )
-            }
+            assert.equal(
+              normalizeHtml(await el.innerHTML()),
+              normalizeHtml(expected),
+              `${name} > ${selector}`
+            )
           }
         }
 
-        await runSubStep(step.steps)
-      }
-    }
+        if ('edit' in step) {
+          await runEdit(step.edit)
+        }
 
-    await stop()
+        if ('expect' in step) {
+          await runExpect(step.expect, step.name)
+        }
+
+        if ('steps' in step) {
+          /** @param {TestHmrStepSteps} sub */
+          const runSubStep = async (sub, i = 0) => {
+            if (Array.isArray(sub)) {
+              let i = 0
+              for (const subsub of sub) {
+                await runSubStep(subsub, i++)
+              }
+            } else if (typeof sub === 'function') {
+              await sub(page)
+            } else {
+              const unknownOptions = new Set(Object.keys(sub))
+              if ('name' in sub) {
+                unknownOptions.delete('name')
+                // TODO
+              }
+              if ('edit' in sub) {
+                unknownOptions.delete('edit')
+                await runEdit(sub.edit)
+              }
+              if ('expect' in sub) {
+                unknownOptions.delete('expect')
+                await runExpect(
+                  sub.expect,
+                  `${step.name} > ${sub.name || `steps[${i}]`}`
+                )
+              }
+              if (unknownOptions.size > 0) {
+                throw new Error(
+                  `Unkown options in step ${step.name}: ${[
+                    // @ts-ignore
+                    ...unknownOptions,
+                  ].join(', ')}`
+                )
+              }
+            }
+          }
+
+          await runSubStep(step.steps)
+        }
+      }
+    } finally {
+      await stop()
+    }
   }
 }
